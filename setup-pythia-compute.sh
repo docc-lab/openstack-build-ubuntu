09784d9c2326ff728eb7a1bf70f7813e8d87e368
Emre Ates <ates@bu.edu>
2020-02-26 11:02:19 -0500

    Added rust setup to the compute node as well

--- setup-pythia-compute.sh
+++ setup-pythia-compute.sh
@@ -55,6 +55,16 @@ chmod -R o+rwX /opt/
 maybe_install_packages redis-server python-redis python3-redis python3-pip
 service_start redis
 
+echo "*** Installing Rust"
+maybe_install_packages python3-pip
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+rustup component add rls
+
+cargo install --path /local/reconstruction
+echo "*** Finished installing Rust"
+
 echo -e 'nova\tALL=(ALL)\tNOPASSWD: ALL' >> /etc/sudoers
 
 profiler_conf=$(cat <<END
