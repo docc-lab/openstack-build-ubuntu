990789928c13206eb7eeadbf2913ef8c55f85f59
RoyZhang7 <royzhangno7@gmail.com>
2022-11-01 17:07:00 -0400

    fix ln path

--- setup-pythia.sh
+++ setup-pythia.sh
@@ -100,7 +100,7 @@ su $CURUSR -c "cargo update --manifest-path /local/pythia/pythia_server/Cargo.to
 su $CURUSR -c "cargo install --locked --path /local/pythia"
 su $CURUSR -c "cargo install --locked --path /local/pythia/pythia_server"
 sudo ln -s /users/$CURUSR/.cargo/bin/pythia_server /usr/local/bin/
-sudo ln -s /local/pythia /users/$CURUSR
+sudo ln -s /local/pythia /users/$CURUSR/
 sudo ln -s /local/dotfiles /users/$CURUSR/
 
 mkdir -p /opt/stack/manifest
