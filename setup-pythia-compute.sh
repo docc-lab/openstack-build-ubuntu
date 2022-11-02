147613a920fd45e40adf152163a6d692b1e688bf
royzhang <Zhaoqi.Zhang@tufts.edu>
2022-11-01 22:03:10 -0400

    change user to geniusesr and remove old repo

--- setup-pythia-compute.sh
+++ setup-pythia-compute.sh
@@ -60,6 +60,11 @@ do
     cd /local
 done
 
+# Remove reconstruction repo & clone new pythia repo
+echo "y" | rm -r /local/reconstruction/
+echo "y" | rm -i /users/geniuser/reconstruction
+git clone https://github.com/docc-lab/pythia.git
+
 mkdir -p /opt/stack/manifest
 chmod -R g+rwX /opt/
 chmod -R o+rwX /opt/
@@ -69,18 +74,20 @@ service_start redis
 maybe_install_packages python3-pip
 
 # Bring back rustup for compilation error
-su toslali -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
+su geniuser -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
 source $HOME/.cargo/env
 rustup update stable
 echo "**** Mert updating rust for match compile error ***"
 
 
-chown toslali -R /local/reconstruction
-su toslali -c "cargo update --manifest-path /local/reconstruction/Cargo.toml -p lexical-core"
-su toslali -c "cargo update --manifest-path /local/reconstruction/pythia_server/Cargo.toml -p lexical-core"
-su toslali -c "cargo install --locked --path /local/reconstruction"
-su toslali -c "cargo install --locked --path /local/reconstruction/pythia_server"
-sudo ln -s /users/toslali/.cargo/bin/pythia_server /usr/local/bin/
+chown geniuser -R /local/pythia
+su geniuser -c "cargo update --manifest-path /local/pythia/Cargo.toml -p lexical-core"
+su geniuser -c "cargo update --manifest-path /local/pythia/pythia_server/Cargo.toml -p lexical-core"
+su geniuser -c "cargo install --locked --path /local/pythia"
+su geniuser -c "cargo install --locked --path /local/pythia/pythia_server"
+sudo ln -s /users/geniuser/.cargo/bin/pythia_server /usr/local/bin/
+sudo ln -s /local/pythia /users/geniuser/
+sudo ln -s /local/dotfiles /users/geniuser/
 
 echo -e 'nova\tALL=(ALL)\tNOPASSWD: ALL' >> /etc/sudoers
 
@@ -133,7 +140,7 @@ sudo systemctl start pythia.service
 touch $OURDIR/setup-pythia-compute-done
 logtend "pythia-compute"
 
-chown toslali -R /local
-su toslali -c 'cd /local/dotfiles; ./setup_cloudlab.sh'
+chown geniuser -R /local
+su geniuser -c 'cd /local/dotfiles; ./setup_cloudlab.sh'
 
 exit 0
--- setup-pythia.sh
+++ setup-pythia.sh
@@ -64,6 +64,11 @@ do
     cd /local
 done
 
+# Remove reconstruction repo & clone new pythia repo
+echo "y" | rm -r /local/reconstruction/
+echo "y" | rm -i /users/geniuser/reconstruction
+git clone https://github.com/docc-lab/pythia.git
+
 PHOSTS=""
 mkdir -p $OURDIR/pssh.setup-pythia.stdout $OURDIR/pssh.setup-pythia.stderr
 
@@ -80,19 +85,21 @@ maybe_install_packages python3-pip
 
 # Bring back rustup for compilation error
 
-su toslali -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
+su geniuser -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
 
 source $HOME/.cargo/env
 rustup update stable
 echo "**** Mert updating rust for match compile error ***"
 
 
-chown toslali -R /local/reconstruction
-su toslali -c "cargo update --manifest-path /local/reconstruction/Cargo.toml -p lexical-core"
-su toslali -c "cargo update --manifest-path /local/reconstruction/pythia_server/Cargo.toml -p lexical-core"
-su toslali -c "cargo install --locked --path /local/reconstruction"
-su toslali -c "cargo install --locked --path /local/reconstruction/pythia_server"
-sudo ln -s /users/toslali/.cargo/bin/pythia_server /usr/local/bin/
+chown geniuser -R /local/pythia
+su geniuser -c "cargo update --manifest-path /local/pythia/Cargo.toml -p lexical-core"
+su geniuser -c "cargo update --manifest-path /local/pythia/pythia_server/Cargo.toml -p lexical-core"
+su geniuser -c "cargo install --locked --path /local/pythia"
+su geniuser -c "cargo install --locked --path /local/pythia/pythia_server"
+sudo ln -s /users/geniuser/.cargo/bin/pythia_server /usr/local/bin/
+sudo ln -s /local/pythia /users/geniuser/
+sudo ln -s /local/dotfiles /users/geniuser/
 
 mkdir -p /opt/stack/manifest
 chmod -R g+rwX /opt/
@@ -207,7 +214,7 @@ sudo systemctl start pythia.service
 touch $OURDIR/setup-pythia-done
 logtend "pythia"
 
-chown toslali -R /local
-su toslali -c 'cd /local/dotfiles; ./setup_cloudlab.sh'
+chown geniuser -R /local
+su geniuser -c 'cd /local/dotfiles; ./setup_cloudlab.sh'
 
 exit 0
