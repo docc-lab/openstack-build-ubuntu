6426f6b234987010be84a2057d4d30338884c5e8
Zhaoqi(Roy) Zhang <Zhaoqi.Zhang@tufts.edu>
2022-11-18 16:56:02 -0500

    fix typo in PR #11

--- setup-pythia-compute.sh
+++ setup-pythia-compute.sh
@@ -61,7 +61,6 @@ do
     git checkout $(git status | head -n 1 | awk '{print $3}') -f
     # # GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i /local/.ssh/$repo" git pull
     git pull
-    git pull
     cd /local
 done
 
@@ -69,7 +68,7 @@ done
 echo "y" | rm -r /local/reconstruction/
 sudo echo "y" | rm -i /users/geniuser/reconstruction
 git clone https://github.com/docc-lab/pythia.git
-chmod -R 777 /users/geniuser/reconstruction
+chmod -R 777 /local/pythia
 
 mkdir -p /opt/stack/manifest
 chmod -R g+rwX /opt/
